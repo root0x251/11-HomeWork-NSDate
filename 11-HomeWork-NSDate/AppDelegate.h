f746f9dbb861cdd0514e1d7b5d73ddf736a6910b
Slava <rtootik@gmail.com>
2017-03-22 14:44:22 +0300

    Initial Commit

---
+++ 11-HomeWork-NSDate/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  11-HomeWork-NSDate
+//
+//  Created by Slava on 22.03.17.
+//  Copyright © 2017 Slava. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 11-HomeWork-NSDate/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  11-HomeWork-NSDate
+//
+//  Created by Slava on 22.03.17.
+//  Copyright © 2017 Slava. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
